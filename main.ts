6f39470ff7e75aab033ee20c5e8ebc442610c69e
Abdullah Arif <160360851+AbdullahArif17@users.noreply.github.com>
2024-05-07 22:03:46 +0500

    update

--- main.ts
+++ main.ts
@@ -1,76 +1,61 @@
 #! /usr/bin/env node
-import inquirer from "inquirer"
+import inquirer from "inquirer";
 import Choices from "inquirer/lib/objects/choices.js";
-import chalk from 'chalk';
+import chalk from "chalk";
 
-let myBalance =  10000;
+let myBalance = 10000;
 
 let pinCode = 1234;
 
-let pinAnswer = await inquirer.prompt(
-    [
+let pinAnswer = await inquirer.prompt([
+  {
+    name: "pin",
+    message: "Enter your pin",
+    type: "number",
+  },
+]);
+
+if (pinAnswer.pin === pinCode) {
+  console.log(chalk.green("\t Correct pin code!"));
+  console.log(chalk.greenBright("\t Your current balance is :" + myBalance));
+
+  let operationAns = await inquirer.prompt([
     {
- name: "pin",
- message: "Enter your pin",
- type: "number"
-    }
-]
-);
+      name: "operation",
+      message: "Please select an option",
+      type: "list",
+      choices: ["Withdraw", "Check Balance", "Fast Cash"],
+    },
+  ]);
 
-if(pinAnswer.pin === pinCode){
-    console.log(chalk.green('\t Correct pin code!'))
-    console.log(chalk.greenBright('\t Your current balance is :' + myBalance ))
-
-    let operationAns = await inquirer.prompt(
-        [
-            {
-                name: 'operation',
-                message: 'Please select an option',
-                type: "list",
-                choices: ['Withdraw', 'Check Balance', 'Fast Cash']
-            }
-        ]
-    );
-    
-    if(operationAns.operation === 'Withdraw'){
-        let amountAns = await inquirer.prompt(
-            [
-                {
-                    name:'amount',
-                    message:'Enter your amount',
-                    type: 'number'
-                }
-            ]
-        );
-    if(amountAns.amount <= myBalance){
-       let newBalance = myBalance -= amountAns.amount 
-        console.log(chalk.green('Your remaining balance is: ' +  newBalance));
-    }else{
-        console.log(chalk.red('ERROR, Enter valid amount please'))
+  if (operationAns.operation === "Withdraw") {
+    let amountAns = await inquirer.prompt([
+      {
+        name: "amount",
+        message: "Enter your amount",
+        type: "number",
+      },
+    ]);
+    if (amountAns.amount <= myBalance) {
+      let newBalance = (myBalance -= amountAns.amount);
+      console.log(chalk.green("Your remaining balance is: " + newBalance));
+    } else {
+      console.log(chalk.red("ERROR, Enter valid amount please"));
     }
-    
-    }
-else if(operationAns.operation === 'Check Balance'){
-    console.log(chalk.green('Your account balance is:  ' +  myBalance))
-}
-else if(operationAns.operation === 'Fast Cash'){
-    let fastCash = await inquirer.prompt(
-        [
-            {
-               name:'fast',
-               message:'Select Amount',
-               type: "list",
-               choices: [1000, 2000 , 5000 , 10000]
-            }
-        ]
-    );
-    myBalance -= fastCash.fast
+  } else if (operationAns.operation === "Check Balance") {
+    console.log(chalk.green("Your account balance is:  " + myBalance));
+  } else if (operationAns.operation === "Fast Cash") {
+    let fastCash = await inquirer.prompt([
+      {
+        name: "fast",
+        message: "Select Amount",
+        type: "list",
+        choices: [1000, 2000, 5000, 10000],
+      },
+    ]);
+    myBalance -= fastCash.fast;
     console.log(chalk.green(`Your remaining balance is: ${myBalance}`));
-
+  }
+} else {
+  console.log(chalk.redBright("Not vaild pin code"));
 }
-}else{
-    console.log(chalk.redBright('Not vaild pin code'))
-};
-
-
-
